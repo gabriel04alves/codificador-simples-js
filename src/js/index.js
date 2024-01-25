daf2eda6aaf997d43b32f2957e87967ae32be90c
Gabriel Alves <gabriel52.alves@gmail.com>
2024-01-25 17:35:35 -0300

    feat: Criptografia

--- src/js/index.js
+++ src/js/index.js
@@ -10,7 +10,27 @@ document.getElementById('criptografar').addEventListener('click', function() {
     
     console.log(contentInput);
     
-    responseDiv.innerHTML = contentInput;
+    const keys = {
+        'e': 'enter',
+        'i': 'imes',
+        'a': 'ai',
+        'o': 'ober',
+        'u': 'ufat',
+        '0': 'sTkP',
+        '1': 'qRjL',
+        '2': 'pWgH',
+        '3': 'kUyZ',
+        '4': 'aXnO',
+        '5': 'vIcF',
+        '6': 'rNzE',
+        '7': 'lMxQ',
+        '8': 'bGhJ',
+        '9': 'dYtA',
+    }
+
+    const encryptedContent = contentInput.split('').map(character => keys[character] || character).join('');
+
+    responseDiv.innerHTML = encryptedContent;
     let responseContainer = document.getElementById('msg-main')
     responseContainer.appendChild(responseDiv)
 });
