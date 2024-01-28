b8acaae9ab4bf18f8e3ef5d3cfad7b36df99a076
Gabriel Alves <gabriel52.alves@gmail.com>
2024-01-28 12:33:40 -0300

    feat: Copiar texto

--- src/js/index.js
+++ src/js/index.js
@@ -18,12 +18,12 @@ document.getElementById('criptografar').addEventListener('click', function() {
         .replace(/6/g, "fsd")
         .replace(/7/g, "sdf")
         .replace(/8/g, "ioi")
-        .replace(/9/g, "fsd")      
+        .replace(/9/g, "tsx")      
 
     document.getElementById('msg-main').innerHTML = 
-        `<p class='res-text'>${encryptedContent}</p> 
+        `<p class='res-text' id='res-text'>${encryptedContent}</p> 
         <div class='copy'>
-            <button class='copy-btn' id='copy'>Copiar </button> 
+            <button class='copy-btn' id='copy' onclick="copy()">Copiar </button> 
         </div>
         `;
     let responseContainer = document.getElementById('msg-main')
@@ -48,9 +48,31 @@ document.getElementById('descriptografar').addEventListener('click', function() 
         .replace(/fsd/g, "6")
         .replace(/sdf/g, "7")
         .replace(/ioi/g, "8")
-        .replace(/fsd/g, "9")
+        .replace(/tsx/g, "9")
 
-    document.getElementById('msg-main').innerHTML = decryptedContent;
+    document.getElementById('msg-main').innerHTML = 
+        `<p class='res-text' id='res-text'>${decryptedContent}</p> 
+        <div class='copy'>
+            <button class='copy-btn' id='copy' onclick="copy()">Copiar </button> 
+        </div>`;
     let responseContainer = document.getElementById('msg-main')
     responseContainer.appendChild(responseDiv)
 });
+
+function copy() {
+    contentCopy = document.getElementById('res-text');
+
+    textToCopy = contentCopy.innerText;
+    toTransfer = document.createElement("textarea");
+    toTransfer.value = textToCopy;
+    document.body.appendChild(toTransfer);
+    toTransfer.select();
+    document.execCommand("copy");
+    document.body.removeChild(toTransfer);
+    Swal.fire({
+        icon: 'success',
+        title: 'Texto copiado!',
+        showConfirmButton: false,
+        timer: 1000
+      });
+}
